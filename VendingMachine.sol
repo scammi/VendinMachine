eca4911550d5cad645e194158936e5119d12d1c1
scammi <scammi@gmail.com>
2019-03-02 18:42:32 -0300

    enchange & to much logic added

--- VendingMachine.sol
+++ VendingMachine.sol
@@ -2,12 +2,36 @@ pragma solidity ^0.5.0;
 
 contract VendingMachine{ 
     
-    event payed(address _client, address _destinatary, uint value);
+    event payed(address _client, address _destinatary, uint _amountPayed);
     
-    function forward(address payable _destinatary)public payable{
-        _destinatary.transfer(msg.value);
-        emit payed(msg.sender, _destinatary, msg.value);
+    function forward(address payable _destinatary, uint _price)public payable{
+        require(msg.value >= _price, "Not enough was payed.");
+    
+        uint amountPayed = msg.value;
         
+        if (amountPayed > _price){
+            uint change = (amountPayed - _price);
+            
+            msg.sender.transfer(change);
+            
+            _destinatary.transfer(_price);
+        
+        } else {
+        _destinatary.transfer(amountPayed);
+    
+        }
+        
+    emit payed(msg.sender, _destinatary, msg.value);
+
     }
     
 }
+
+
+
+    
+
+    
+
+
+
