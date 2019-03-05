d9a626987c0854179e96c8096b29f149b9baf70a
scammi <scammi@gmail.com>
2019-03-05 19:41:28 -0300

    added GaveChange event, fixed indentation

--- contracts/VendingMachine.sol
+++ contracts/VendingMachine.sol
@@ -1,57 +1,46 @@
 pragma solidity ^0.5.0;
 
 /**
- * @title VendingMachine 
+ * @title VendingMachine
  * @dev see https://github.com/scammi/VendingMachine
 */
 import "node_modules/zos-lib/contracts/Initializable.sol";
 
-contract VendingMachine is Initializable{ 
-   
-//Triggers event when payment is completed
-    event payed(address _client, address _destinatary, uint _amountPay);
 
-/**
-*@dev contructor initialize sets owner of the contract to the deployer
-*/
-    function initialize() initializer public {
-       address owner = msg.sender;  
-    }
+contract VendingMachine is Initializable {
+
+//Triggers event when payment is completed
+    event Paid(address _client, address _destinatary, uint _amountPay);
+    event GaveChange(address _client, uint _change);
 /**
 *@dev fowards payments to owner of service
-*triggers event when successfully paid
-*returns change when payment exceeds price 
+*triggers event when successfully foward payment
+*returns change when payment exceeds price
 *@param _destinatary address belongs to the owner of the service
 *@param _price uint the price of the service
-*/ 
-    function forward(address payable _destinatary, uint _price) public payable{
+*/
+
+    function forward(address payable _destinatary, uint _price) public payable {
         require(msg.value >= _price, "Not enough was payed.");
-    
+
         uint amountPaid = msg.value;
-        
-        if (amountPaid > _price){
+
+        if (amountPaid > _price) {
             uint change = (amountPaid - _price);
-            
+
             msg.sender.transfer(change);
-            
+
             _destinatary.transfer(_price);
-        
+
+            emit GaveChange(msg.sender, change)
+
         } else {
-        _destinatary.transfer(amountPaid);
-    
+            _destinatary.transfer(amountPaid);
+
         }
-        
-    emit payed(msg.sender, _destinatary, msg.value);
+
+        emit Paid(msg.sender, _destinatary, msg.value);
 
     }
-    
+
 }
-
-
-
-    
-
-    
-
-
-
