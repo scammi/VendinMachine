0d4dc95b4a5afd244d8c55e21275d13a13de25e2
scammi <scammi@gmail.com>
2019-03-15 16:21:43 -0300

    initializer added & tested contract on proxy

--- contracts/VendingMachine.sol
+++ contracts/VendingMachine.sol
@@ -4,11 +4,19 @@ pragma solidity ^0.5.0;
  * @title VendingMachine
  * @dev see https://github.com/scammi/VendingMachine
 */
+import "zos-lib/contracts/Initializable.sol";
 
-contract VendingMachine {
+
+contract VendingMachine is Initializable {
+
+   address owner;
 
 //Triggers event when payment is completed
     event Paid(address _client, address _destinatary, uint _amountPay);
+
+    function initialize() initializer public {
+       owner = msg.sender;
+    }
 
 /**
 *@dev fowards payments to owner of service
